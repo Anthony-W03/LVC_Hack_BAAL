431944284cf703fc0f155ff2807d52b7901ca5c7
Anthony-W03 <anthony.l.white03@gmail.com>
2024-10-19 15:37:29 -0400

    Filling up client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NetworkVisualizer from './components/NetworkVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Network Visualizer</h1>
+      <NetworkVisualizer />
+    </div>
+  );
+}
+
+export default App;
