46350599e172c915c2a06acec2a40ebad1d59fc2
loganmca01 <loganmca01@gmail.com>
2024-10-19 22:07:40 -0400

    script for creating database

--- aws_access/postgres_utils.py
+++ aws_access/postgres_utils.py
@@ -43,3 +43,79 @@ class sqlUtils():
         except:
             return None
         
+
+db = sqlUtils()
+db.connect()
+
+'''
+test1 = db.query(
+"""
+CREATE TABLE useraccounts (
+    id INT PRIMARY KEY,
+    username VARCHAR(255),
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+"""
+)
+print(test1)
+
+test2 = db.query(
+"""
+CREATE TABLE networks (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES useraccounts(id)
+);
+"""
+)
+print(test2)
+
+
+test2 = db.query(
+"""
+CREATE TABLE connections (
+    id INT PRIMARY KEY,
+    user_id INT,
+    network_id INT,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    connection_through INT,
+    linkedin VARCHAR(255),
+    website VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    address VARCHAR(255),
+    employment VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES useraccounts(id),
+    FOREIGN KEY (network_id) REFERENCES networks(id)
+);
+
+"""
+)
+print(test2)
+'''
+
+
+test3 = db.query(
+"""
+SELECT table_name, column_name
+    FROM information_schema.columns
+WHERE table_name IN (
+  SELECT table_name
+    FROM information_schema.tables
+  WHERE table_type = 'BASE TABLE'
+    AND table_schema NOT IN
+        ('pg_catalog', 'information_schema'));
+"""
+)
+print(test3)
+
+
+
+
+
+
